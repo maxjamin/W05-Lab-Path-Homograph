1bb6e6b02906c0dff03496aec176477c6af378b4
Maxjamin <maxjamin@gmail.com>
2020-02-03 18:28:55 -0700

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/***********************************************************************
+* Program:
+*    Lab 05, Homographs
+*    Brother Helfrich, CS 370
+* Author:
+*    John Miller
+*    Bretton Steiner
+*    Henrique Vaz
+*    Tyler Starr
+*    Ben Smith
+* Summary:
+*    Enter a brief description of your program here!
+ ************************************************************************/
+
+
+
+int main()
+{
+
+  return 0;
+}
